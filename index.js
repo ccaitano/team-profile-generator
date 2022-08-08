2fc3a99a46fee44026699fc4f381681400bc897b
ccaitano <cheryl.caitano@gmail.com>
2022-08-07 15:07:47 -1000

    Added Manager Function to index.js

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
 const generateHTML = require('./generateHTML');
 
-const managerQuestions = [
-    {
-        type: "input",
-        message: "Welcome to the Team Profile Generator. Please press 'ENTER' to begin.",
-        name: "welcome"
-    },
-    {
-        type:"input",
-        message: "Please enter the Team Manager's name: ",
-        name: "managerName"
-    },
-    {
-        type: "input",
-        message: "Please enter the Team Manager's employee ID: ",
-        name: "managerID"
-    },
-    {
-        type: "input",
-        message: "Please enter the Team Manager's e-mail address: ",
-        name: "managerEmail"
-    },
-    {
-        type: "input",
-        message: "Please enter the Team Manager's office number: ",
-        name: "managerNumber"
-    },
-    {
-        type: "list",
-        message: "Would you like to add another employee to this team?",
-        name: "addEmployee",
-        choices: ["Add New Engineer", "Add New Intern", "No Other Team Members to Add"],
-        default: "No Other Team Members to Add"
-    }
-]
+var teamMembers = [];
+
+function createManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Welcome to the Team Profile Generator. Please press 'ENTER' to begin.",
+                name: "welcome"
+            },
+            {
+                type:"input",
+                message: "Please enter the Team Manager's name: ",
+                name: "managerName"
+            },
+            {
+                type: "input",
+                message: "Please enter the Team Manager's employee ID: ",
+                name: "managerID"
+            },
+            {
+                type: "input",
+                message: "Please enter the Team Manager's e-mail address: ",
+                name: "managerEmail"
+            },
+            {
+                type: "input",
+                message: "Please enter the Team Manager's office number: ",
+                name: "managerNumber"
+            },
+            {
+                type: "list",
+                message: "Would you like to add another employee to this team?",
+                name: "addEmployee",
+                choices: ["Add New Engineer", "Add New Intern", "No Other Team Members to Add"],
+                default: "No Other Team Members to Add"
+            }
+        ])
+        .then((response) => {
+            var { managerName, managerID, managerEmail, officeNumber } = response;
+            var manager = new Manager(managerName, managerID, managerEmail, officeNumber);
+
+            teamMembers.push(manager);
+        });
+
+}
+
 function writeToFile(data) {
     generateHTML(data);
     console.log(data.managerEmail);
@@ -44,14 +59,4 @@ function writeToFile(data) {
     // )
 }
 
-function init() {
-    inquirer
-        .prompt([
-            ...managerQuestions
-        ])
-        .then((response) => {
-            writeToFile(response);
-        });
-}
-
-init();
+createManager();
